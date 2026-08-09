00e6a3ad6153869e45033bdafe7f269a133512a0
guigrano <guigrano@espol.edu.ec>
2026-08-09 18:20:53 -0500

    Clase Tree y clase Node ahora son una sola clase (TreeNode) para obtener mayor eficiencia en la implementación.
    Creación de clase InteligenciaComputador con las funcionalidades de decidir mejor movimiento y calcular utilidades.
    
    Clase Tablero ahora posee más métodos para manipular el tablero.

--- src/main/java/com/mycompany/main/Tablero.java
+++ src/main/java/com/mycompany/main/Tablero.java
@@ -8,38 +8,59 @@ package com.mycompany.main;
  *
  * @author guill
  */
-    public class Tablero {
-        private Simbolo[][] tablero;
+public class Tablero {
+    private Simbolo[][] tablero;
+    public static final int[][][] LINEAS_GANADORAS = {
+        // 3 Filas
+        {{0, 0}, {0, 1}, {0, 2}}, 
+        {{1, 0}, {1, 1}, {1, 2}},
+        {{2, 0}, {2, 1}, {2, 2}},
+        // 3 Columnas
+        {{0, 0}, {1, 0}, {2, 0}},
+        {{0, 1}, {1, 1}, {2, 1}},
+        {{0, 2}, {1, 2}, {2, 2}},
+        // 2 Diagonales
+        {{0, 0}, {1, 1}, {2, 2}},
+        {{0, 2}, {1, 1}, {2, 0}}
+    };
 
-        public Tablero() {
-            tablero = new Simbolo[3][3];
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) tablero[i][j] = Simbolo.VACIO;
-            }
-        }
-
-        public Simbolo[][] copiarTablero() {
-            Simbolo[][] copiaTablero = new Simbolo[3][3];
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) copiaTablero[i][j] = tablero[i][j];
-            }
-            return copiaTablero;
-        }
-
-        public void marcarCasilla(int fila, int col, Simbolo s) {
-            tablero[fila][col] = s;
-        }
-        
-        // metodo temporal para pruebas
-        public void mostrarTablero() {
-            for (int i = 0; i < 3; i++) {
-                System.out.print('[');
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(tablero[i][j]);
-                    if (j < 2) System.out.print(", ");
-                }
-                System.out.print(']');
-                if (i < 2) System.out.println();
-            }
+    public Tablero() {
+        tablero = new Simbolo[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) tablero[i][j] = Simbolo.VACIO;
         }
     }
+
+    public Tablero copiarTablero() {
+        Tablero copiaTablero = new Tablero();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Simbolo simbolo = tablero[i][j];
+                copiaTablero.marcarCasilla(i, j, simbolo);
+            }
+        }
+        return copiaTablero; 
+    }
+
+    public void marcarCasilla(int fila, int col, Simbolo s) {
+        tablero[fila][col] = s;
+    }
+    
+    public Simbolo getSimboloEnCasilla(int fila, int col) {
+        return tablero[fila][col];
+    }
+
+
+    // metodo temporal para pruebas
+    public void mostrarTablero() {
+        for (int i = 0; i < 3; i++) {
+            System.out.print('[');
+            for (int j = 0; j < 3; j++) {
+                System.out.print(tablero[i][j]);
+                if (j < 2) System.out.print(", ");
+            }
+            System.out.print(']');
+            if (i < 2) System.out.println();
+        }
+    }
+}
